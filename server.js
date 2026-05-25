a0f2971f9fd40601cc0a39072944c34fe10f043e
Edison Quishpe <edison.quishpe02@epn.edu.ec>
2026-05-25 12:41:47 -0500

    feat: setup websocket server with socket.io

---
+++ server.js
@@ -0,0 +1,31 @@
+const { createServer } = require("http");
+const next = require("next");
+const { Server } = require("socket.io");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = createServer((req, res) => {
+    handle(req, res);
+  });
+
+  const io = new Server(server);
+
+  io.on("connection", (socket) => {
+    console.log("Usuario conectado:", socket.id);
+
+    socket.on("chat-message", (message) => {
+      io.emit("chat-message", message);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Usuario desconectado:", socket.id);
+    });
+  });
+
+  server.listen(3000, () => {
+    console.log("Servidor listo en http://localhost:3000");
+  });
+});
