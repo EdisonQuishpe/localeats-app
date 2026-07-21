6c039e6d2fcd3875b1886f682647de4f14dc0c4f
Edison Quishpe <edison.quishpe02@epn.edu.ec>
2026-07-20 19:55:52 -0500

    feat: configure auth service with TCP transport

--- backend/api-gateway/apps/auth-service/src/main.ts
+++ backend/api-gateway/apps/auth-service/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AuthServiceModule } from './auth-service.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthServiceModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AuthServiceModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '127.0.0.1',
+        port: 4001,
+      },
+    },
+  );
+
+  await app.listen();
+
+  console.log('Auth Service ejecutándose en TCP puerto 4001');
 }
+
 bootstrap();
